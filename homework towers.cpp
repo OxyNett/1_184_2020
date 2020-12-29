b421d4594b57f1eaa3b453ee6dbd86f4c06596b5
Dasha <dasha.terentevaa17@mail.com>
2020-12-29 16:04:03 +0300

    Proverka

---
+++ homework towers.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream> 
+using namespace std;
+void hanoi_towers(int n, int from, int to, int bp); //q-количество колец, from-начальное положение колец(1-3), to - конечное положение колец (1-3), bp-промежуточный колышек (1-3) 
+int main() {
+	int n,k, t, bp;
+	cin >> n >> k >> t >> bp;
+	hanoi_towers(n, k, t, bp);
+	return 0;
+}
+void hanoi_towers(int n, int from, int to, int bp) {
+	if (n != 0) {
+		hanoi_towers(n - 1, from, bp, to);
+		cout << "{0}->{1}" << from << " " << to;
+		hanoi_towers(n - 1, bp, to, from);
+	}
+}
+
